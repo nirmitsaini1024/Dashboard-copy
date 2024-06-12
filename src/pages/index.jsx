602472cf38ed315311b5ec0781da70befc99fc52
Nirmit Saini <122887695+nirmitsaini1024@users.noreply.github.com>
2024-06-12 13:02:14 +0530

    Add files via upload

---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as CuttingDepartment } from './CuttingDepartment';
+export { default as ProductionDepartment } from './ProductionDepartment';
+export { default as FinishingDepartment } from './FinishingDepartment';
